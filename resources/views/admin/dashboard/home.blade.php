1e652591e81cb258ede4f5fc59a1e4d529533f54
codewithsabbirIYI <tauhid10030@gmial.com>
2023-10-23 14:17:42 +0600

    template mastaring

---
+++ resources/views/admin/dashboard/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin')
+
+@section('page')
+<div class="row">
+    <div class="col-md-12 welcome_part">
+        <p><span>Welcome Mr.</span> Saidul Islam Uzzal</p>
+    </div>
+</div>
+@endsection
